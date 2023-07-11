543ad93962a56a5a93ba3903e41f0d701ededd16
rmaguado <rmorenoaguado3643@gmail.com>
2023-07-11 16:51:12 +0200

    add models to test

--- models/vit_mae.py
+++ models/vit_mae.py
@@ -1,117 +1,198 @@
-from typing import Optional, Tuple, Union
-
 import torch
-import torch.utils.checkpoint
+import torch.nn as nn
 
-from transformers.models.vit_mae.modeling_vit_mae import ViTMAEPreTrainedModel, ViTMAEForPreTrainingOutput, ViTMAEModel, ViTMAEDecoder
-from transformers import ViTMAEConfig
+from timm.models.vision_transformer import PatchEmbed, Block
 
-class ViTMAE(ViTMAEPreTrainedModel):
-    def __init__(self, config):
-        super().__init__(config)
-        self.config = config
+from util.pos_embed import get_2d_sincos_pos_embed
 
-        self.vit = ViTMAEModel(config)
-        self.decoder = ViTMAEDecoder(config, num_patches=self.vit.embeddings.num_patches)
 
-        # Initialize weights and apply final processing
-        self.post_init()
+class MaskedAutoencoderViT(nn.Module):
+    """ Masked Autoencoder with VisionTransformer backbone
+    """
+    def __init__(self, img_size=224, patch_size=16, in_chans=3,
+                 embed_dim=1024, depth=24, num_heads=16,
+                 decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False):
+        super().__init__()
 
-    def get_input_embeddings(self):
-        return self.vit.embeddings.patch_embeddings
+        # --------------------------------------------------------------------------
+        # MAE encoder specifics
+        self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+        num_patches = self.patch_embed.num_patches
 
-    def _prune_heads(self, heads_to_prune):
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim), requires_grad=False)  # fixed sin-cos embedding
+
+        self.blocks = nn.ModuleList([
+            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+        # --------------------------------------------------------------------------
+
+        # --------------------------------------------------------------------------
+        # MAE decoder specifics
+        self.decoder_embed = nn.Linear(embed_dim, decoder_embed_dim, bias=True)
+
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, decoder_embed_dim))
+
+        self.decoder_pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, decoder_embed_dim), requires_grad=False)  # fixed sin-cos embedding
+
+        self.decoder_blocks = nn.ModuleList([
+            Block(decoder_embed_dim, decoder_num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            for i in range(decoder_depth)])
+
+        self.decoder_norm = norm_layer(decoder_embed_dim)
+        self.decoder_pred = nn.Linear(decoder_embed_dim, patch_size**2 * in_chans, bias=True) # decoder to patch
+        # --------------------------------------------------------------------------
+
+        self.norm_pix_loss = norm_pix_loss
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # initialization
+        # initialize (and freeze) pos_embed by sin-cos embedding
+        pos_embed = get_2d_sincos_pos_embed(self.pos_embed.shape[-1], int(self.patch_embed.num_patches**.5), cls_token=True)
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))
+
+        decoder_pos_embed = get_2d_sincos_pos_embed(self.decoder_pos_embed.shape[-1], int(self.patch_embed.num_patches**.5), cls_token=True)
+        self.decoder_pos_embed.data.copy_(torch.from_numpy(decoder_pos_embed).float().unsqueeze(0))
+
+        # initialize patch_embed like nn.Linear (instead of nn.Conv2d)
+        w = self.patch_embed.proj.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        # timm's trunc_normal_(std=.02) is effectively normal_(std=0.02) as cutoff is too big (2.)
+        torch.nn.init.normal_(self.cls_token, std=.02)
+        torch.nn.init.normal_(self.mask_token, std=.02)
+
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def patchify(self, imgs):
         """
-        Prunes heads of the model. heads_to_prune: dict of {layer_num: list of heads to prune in this layer} See base
-        class PreTrainedModel
+        imgs: (N, 3, H, W)
+        x: (N, L, patch_size**2 *3)
         """
-        for layer, heads in heads_to_prune.items():
-            self.encoder.layer[layer].attention.prune_heads(heads)
+        p = self.patch_embed.patch_size[0]
+        assert imgs.shape[2] == imgs.shape[3] and imgs.shape[2] % p == 0
 
-    def patchify(self, pixel_values):
+        h = w = imgs.shape[2] // p
+        x = imgs.reshape(shape=(imgs.shape[0], 3, h, p, w, p))
+        x = torch.einsum('nchpwq->nhwpqc', x)
+        x = x.reshape(shape=(imgs.shape[0], h * w, p**2 * 3))
+        return x
+
+    def unpatchify(self, x):
         """
-        Args:
-            pixel_values (`torch.FloatTensor` of shape `(batch_size, num_channels, height, width)`):
-                Pixel values.
-
-        Returns:
-            `torch.FloatTensor` of shape `(batch_size, num_patches, patch_size**2 * num_channels)`:
-                Patchified pixel values.
+        x: (N, L, patch_size**2 *3)
+        imgs: (N, 3, H, W)
         """
-        patch_size, num_channels = self.config.patch_size, self.config.num_channels
-        # sanity checks
-        if (pixel_values.shape[2] != pixel_values.shape[3]) or (pixel_values.shape[2] % patch_size != 0):
-            raise ValueError("Make sure the pixel values have a squared size that is divisible by the patch size")
-        if pixel_values.shape[1] != num_channels:
-            raise ValueError(
-                "Make sure the number of channels of the pixel values is equal to the one set in the configuration"
-            )
+        p = self.patch_embed.patch_size[0]
+        h = w = int(x.shape[1]**.5)
+        assert h * w == x.shape[1]
+        
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, 3))
+        x = torch.einsum('nhwpqc->nchpwq', x)
+        imgs = x.reshape(shape=(x.shape[0], 3, h * p, h * p))
+        return imgs
 
-        # patchify
-        batch_size = pixel_values.shape[0]
-        num_patches_one_direction = pixel_values.shape[2] // patch_size
-        patchified_pixel_values = pixel_values.reshape(
-            batch_size, num_channels, num_patches_one_direction, patch_size, num_patches_one_direction, patch_size
-        )
-        patchified_pixel_values = torch.einsum("nchpwq->nhwpqc", patchified_pixel_values)
-        patchified_pixel_values = patchified_pixel_values.reshape(
-            batch_size, num_patches_one_direction * num_patches_one_direction, patch_size**2 * num_channels
-        )
-        return patchified_pixel_values
-
-    def unpatchify(self, patchified_pixel_values):
+    def random_masking(self, x, mask_ratio):
         """
-        Args:
-            patchified_pixel_values (`torch.FloatTensor` of shape `(batch_size, num_patches, patch_size**2 * num_channels)`:
-                Patchified pixel values.
-
-        Returns:
-            `torch.FloatTensor` of shape `(batch_size, num_channels, height, width)`:
-                Pixel values.
+        Perform per-sample random masking by per-sample shuffling.
+        Per-sample shuffling is done by argsort random noise.
+        x: [N, L, D], sequence
         """
-        patch_size, num_channels = self.config.patch_size, self.config.num_channels
-        num_patches_one_direction = int(patchified_pixel_values.shape[1] ** 0.5)
-        # sanity check
-        if num_patches_one_direction**2 != patchified_pixel_values.shape[1]:
-            raise ValueError("Make sure that the number of patches can be squared")
+        N, L, D = x.shape  # batch, length, dim
+        len_keep = int(L * (1 - mask_ratio))
+        
+        noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
+        
+        # sort noise for each sample
+        ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
+        ids_restore = torch.argsort(ids_shuffle, dim=1)
 
-        # unpatchify
-        batch_size = patchified_pixel_values.shape[0]
-        patchified_pixel_values = patchified_pixel_values.reshape(
-            batch_size,
-            num_patches_one_direction,
-            num_patches_one_direction,
-            patch_size,
-            patch_size,
-            num_channels,
-        )
-        patchified_pixel_values = torch.einsum("nhwpqc->nchpwq", patchified_pixel_values)
-        pixel_values = patchified_pixel_values.reshape(
-            batch_size,
-            num_channels,
-            num_patches_one_direction * patch_size,
-            num_patches_one_direction * patch_size,
-        )
-        return pixel_values
+        # keep the first subset
+        ids_keep = ids_shuffle[:, :len_keep]
+        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
 
-    def forward_loss(self, pixel_values, pred, mask):
+        # generate the binary mask: 0 is keep, 1 is remove
+        mask = torch.ones([N, L], device=x.device)
+        mask[:, :len_keep] = 0
+        # unshuffle to get the binary mask
+        mask = torch.gather(mask, dim=1, index=ids_restore)
+
+        return x_masked, mask, ids_restore
+
+    def forward_encoder(self, x, mask_ratio):
+        # embed patches
+        x = self.patch_embed(x)
+
+        # add pos embed w/o cls token
+        x = x + self.pos_embed[:, 1:, :]
+
+        # masking: length -> length * mask_ratio
+        x, mask, ids_restore = self.random_masking(x, mask_ratio)
+
+        # append cls token
+        cls_token = self.cls_token + self.pos_embed[:, :1, :]
+        cls_tokens = cls_token.expand(x.shape[0], -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        # apply Transformer blocks
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.norm(x)
+
+        return x, mask, ids_restore
+
+    def forward_decoder(self, x, ids_restore):
+        # embed tokens
+        x = self.decoder_embed(x)
+
+        # append mask tokens to sequence
+        mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
+        x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token
+        x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
+        x = torch.cat([x[:, :1, :], x_], dim=1)  # append cls token
+
+        # add pos embed
+        x = x + self.decoder_pos_embed
+
+        # apply Transformer blocks
+        for blk in self.decoder_blocks:
+            x = blk(x)
+        x = self.decoder_norm(x)
+
+        # predictor projection
+        x = self.decoder_pred(x)
+
+        # remove cls token
+        x = x[:, 1:, :]
+
+        return x
+
+    def forward_loss(self, imgs, pred, mask):
         """
-        Args:
-            pixel_values (`torch.FloatTensor` of shape `(batch_size, num_channels, height, width)`):
-                Pixel values.
-            pred (`torch.FloatTensor` of shape `(batch_size, num_patches, patch_size**2 * num_channels)`:
-                Predicted pixel values.
-            mask (`torch.FloatTensor` of shape `(batch_size, sequence_length)`):
-                Tensor indicating which patches are masked (1) and which are not (0).
-
-        Returns:
-            `torch.FloatTensor`: Pixel reconstruction loss.
+        imgs: [N, 3, H, W]
+        pred: [N, L, p*p*3]
+        mask: [N, L], 0 is keep, 1 is remove, 
         """
-        target = self.patchify(pixel_values)
-        if self.config.norm_pix_loss:
+        target = self.patchify(imgs)
+        if self.norm_pix_loss:
             mean = target.mean(dim=-1, keepdim=True)
             var = target.var(dim=-1, keepdim=True)
-            target = (target - mean) / (var + 1.0e-6) ** 0.5
+            target = (target - mean) / (var + 1.e-6)**.5
 
         loss = (pred - target) ** 2
         loss = loss.mean(dim=-1)  # [N, L], mean loss per patch
@@ -119,55 +200,8 @@ class ViTMAE(ViTMAEPreTrainedModel):
         loss = (loss * mask).sum() / mask.sum()  # mean loss on removed patches
         return loss
 
-    def forward(
-        self,
-        pixel_values: Optional[torch.FloatTensor] = None,
-        noise: Optional[torch.FloatTensor] = None,
-        head_mask: Optional[torch.FloatTensor] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None,
-    ) -> Union[Tuple, ViTMAEForPreTrainingOutput]:
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-        outputs = self.vit(
-            pixel_values,
-            noise=noise,
-            head_mask=head_mask,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-        latent = outputs.last_hidden_state
-        ids_restore = outputs.ids_restore
-        mask = outputs.mask
-
-        decoder_outputs = self.decoder(latent, ids_restore)
-        logits = decoder_outputs.logits  # shape (batch_size, num_patches, patch_size*patch_size*num_channels)
-
-        loss = self.forward_loss(pixel_values, logits, mask)
-
-        if not return_dict:
-            output = (logits, mask, ids_restore) + outputs[2:]
-            return ((loss,) + output) if loss is not None else output
-
-        return ViTMAEForPreTrainingOutput(
-            loss=loss,
-            logits=logits,
-            mask=mask,
-            ids_restore=ids_restore,
-            hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions,
-        )
-
-testConfig = ViTMAEConfig(**{
-    "hidden_size" : 768,
-    "num_hidden_layers" : 12,
-    "num_attention_heads" : 12,
-    "intermediate_size" : 3072,
-    "image_size" : 224,
-    "patch_size" : 16,
-    "num_channels" : 1
-})
-testModel = ViTMAE(testConfig)
+    def forward(self, imgs, mask_ratio=0.75):
+        latent, mask, ids_restore = self.forward_encoder(imgs, mask_ratio)
+        pred = self.forward_decoder(latent, ids_restore)  # [N, L, p*p*3]
+        loss = self.forward_loss(imgs, pred, mask)
+        return loss, pred, mask
---
+++ train_vitmae.py
@@ -0,0 +1,57 @@
+import json
+from libtiff import TIFF
+from glob import glob
+import numpy as np
+import torch
+from torch.optim import AdamW
+from transformers import ViTMAEConfig
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
+from models.vit_mae import ViTMAE
+
+def load_images():
+    img_dirs = glob("./tempdata/tiff_images/*.tif")
+    images = [
+        TIFF.open(img_dir, mode='r').read_image() for img_dir in img_dirs
+    ]
+    images_standard = [
+        (img / max( np.max(img), abs(np.min(img)) ) + 1 ) / 2 for img in images
+    ]
+    images = np.array(images_standard, dtype=np.float32)
+    
+    images = np.expand_dims(images, axis=1)
+    images = torch.tensor(images)
+    return images
+
+images = load_images()
+
+with open("config.json") as file:
+    config = json.load(file)
+lr = config["trainer"]["learning_rate"]
+
+model_config = ViTMAEConfig(
+    **config["model"]
+)
+model = ViTMAE(model_config).float()
+optim = AdamW(model.parameters(), lr=lr)
+
+device = torch.device("cpu")
+model.to(device)
+images.to(device)
+
+total_epochs = 100
+model.train()
+loop = tqdm(range(total_epochs), leave=True, ascii=" >=")
+
+for epoch_number in loop:
+    model.zero_grad()
+    outputs = model.forward(images)
+    loss = outputs.loss
+    loss.backward()
+    loop.set_postfix({
+        "loss":f"{loss.item():.2f}"
+    })
+    optim.step()
+
