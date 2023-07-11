9f82f769cd1573a48902b70b78c15b46aa9d7cbc
rmaguado <rmorenoaguado3643@gmail.com>
2023-07-11 16:51:01 +0200

    test with dataset

--- datatest.py
+++ datatest.py
@@ -2,9 +2,8 @@ from libtiff import TIFF
 from glob import glob
 
 img_dirs = glob("./tempdata/tiff_images/*.tif")
-print(len(img_dirs))
-
-tif = TIFF.open('./tempdata/tiff_images/ID_0000_AGE_0060_CONTRAST_1_CT.tif', mode='r')
-image = tif.read_image()
-
-print(image.shape)
+images = np.array(
+    [
+        TIFF.open(img_dir, mode='r').read_image() for img_dir in img_dirs
+    ]
+)
