27262c42234334e76294b182cf0ff21cd7bb2504
rmaguado <rmorenoaguado3643@gmail.com>
2023-07-26 11:48:39 +0200

    class combines ct and mri datasets

--- train_vitmae.py
+++ train_vitmae.py
@@ -1,7 +1,8 @@
 import json
 import numpy as np
 from torch.optim import AdamW
-from torch.utils.data import Dataset, DataLoader
+from dataloader import Dataloader
+from tqdm import tqdm
 
 from models.VITMAE.vit_mae import MaskedAutoencoderViT
 from trainer import Trainer
@@ -10,20 +11,43 @@ with open("config.json") as file:
     config = json.load(file)
 lr = config["trainer"]["learning_rate"]
 
-class testData(Dataset):
-    def __init__(self):
-        self.images = np.load(
-            "/nfs/home/clruben/workspace/nst/tempdata/preprocessed/test_batch.npz"
-        )["arr_0"]
+rootpath = "/nfs/home/clruben/workspace/nst/data/preprocessed"
 
+class MaskingAE_Dataloader:
+    def __init__(self, source, dataset):
+        """Pools together MRI and CT scans for masked modeling.
+        """
+        self.loaders = [
+            Dataloader(source, dataset, "CT"),
+            Dataloader(source, dataset, "MRI")
+        ]
+        self.equal_num_examples = min(
+            len(self.ct_loader),
+            len(self.mri_loader)
+        ) * 2
+        self.mode = True
+        self.counter = 0
+        self.masking_ratio = 0.75
     def __len__(self):
-        return len(self.images)
-    def __getitem__(self, idx):
-        return self.images[idx]
+        return self.equal_num_examples
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.mode = not self.mode
+        if self.counter == self.equal_num_examples:
+            self.counter = 0
+        next_item = next(self.loaders[self.mode])
+        mode = ["CT", "MRI"][self.mode]
+        return next_item, mode, self.masking_ratio
 
-testloader = DataLoader(
-    testData(),
-    **config["dataloader"]
+train_loader = MaskingAE_Dataloader(
+    rootpath,
+    'train'
+)
+
+test_loader = MaskingAE_Dataloader(
+    rootpath,
+    'test'
 )
 
 model = MaskedAutoencoderViT(**config["model"])
@@ -36,6 +60,6 @@ trainer = Trainer(
     **config["trainer"]
 )
 
-trainer.train(testloader, testloader)
+trainer.train(train_loader, test_loader)
 #model.mode = "nst"
 #trainer.train(testloader, testloader)
