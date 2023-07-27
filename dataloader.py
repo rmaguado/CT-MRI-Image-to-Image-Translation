2feee2eb946244c254919fb02f8b0f53374ec3c5
rmaguado <rmorenoaguado3643@gmail.com>
2023-07-27 16:20:54 +0200

    data augmentation

--- dataloader.py
+++ dataloader.py
@@ -3,17 +3,50 @@ from glob import glob
 import os
 
 class Dataloader:
-    def __init__(self, source, dataset, mode):
+    def __init__(self, source, dataset, mode, data_augmentation=True):
         source_dir = os.path.join(source, dataset)
         self.source_dir = source_dir
         self.mode = mode
-        data_dir = glob(
+        self.data_augmentation = data_augmentation
+
+        paths_to_database = glob(
             os.path.join(source_dir, mode, "*.npy")
-        )[0]
-        data_shape = tuple([int(x) for x in os.path.basename(data_dir).replace(".npy","").split("_")])
-        self.batches = np.memmap(data_dir, dtype=np.float32, mode='r', shape=data_shape)
+        )
+        if not paths_to_database:
+            raise ValueError("Data not found. Check source directory provided.")
+
+        data_dir = paths_to_database[0]
+        data_shape = tuple(
+            [
+                int(x) for x in os.path.basename(
+                    data_dir
+                ).replace(".npy","").split("_")
+            ]
+        )
+        self.batches = np.memmap(
+            data_dir,
+            dtype=np.float32,
+            mode='r',
+            shape=data_shape
+        )
         self.counter = 0
         self.total_batches = data_shape[0]
+
+    def data_augmentation(self, batch_data):
+        """Reflects and rotates the image at random.
+
+        Args:
+            batch_data (np.ndarray): Shape (B, C, H, W)
+        Returns:
+            (nd.array) : Transformed batch_data (B, C, H, W)
+        """
+        num_rotations = np.random.randint(4)
+        batch_data = np.rot90(batch_data, num_rotations, axes=(2, 3))
+
+        if np.random.random() < 0.5:
+            batch_data = np.flip(batch_data, axis=3)
+
+        return batch_data
 
     def __len__(self):
         return self.total_batches
@@ -24,6 +57,8 @@ class Dataloader:
             self.counter = 0
         batch = self.batches[self.counter]
         self.counter += 1
+        if self.data_augmentation:
+            return self.data_augmentation(batch)
         return batch
 
 if __name__ == "__main__":
