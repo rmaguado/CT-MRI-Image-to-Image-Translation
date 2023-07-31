afdb400ab1df66a07d3cd126362908f5b842776c
rmaguado <rmorenoaguado3643@gmail.com>
2023-07-31 15:24:49 +0200

    rename and abstract

--- database_split.py
+++ database_split.py
@@ -1,59 +1,49 @@
+"""
+This script is used to split the TCIA dataset into train, test, and validation.
+Slices from the same patient are spread out into different batches.
+"""
+
 import os
 import numpy as np
-from PIL import Image
 import nibabel as nib
 import pandas as pd
 import random
 from tqdm import tqdm
 import cv2
 
-dataset_root = "/nfs/rwork/DATABASES_OPENSOURCE/TCIA_anon/Selected_CTs.csv"
+dataset_root = "/nfs/rwork/DATABASES_OPENSOURCE/TCIA_anon_temp/Selected_CTs.csv"
 save_root = "/nfs/home/clruben/workspace/nst/data/"
-mode = "CT"
-
-sources = pd.read_csv(dataset_root)
-
-total_slices = 0
-loop = tqdm(range(len(sources)))
-for i in loop:
-    try:
-        nii_ = nib.load(
-            sources.iloc[i]["FinalPath"]
-        )
-        image = nii_.get_fdata()
-    except:
-        continue
-    if mode == "MRI":
-        if len(image.shape) > 3 and np.min(image) < 0:
-            continue
-    total_slices += min(image.shape)
-
-print(f"Total slices: {total_slices}")
-
-BATCH_SIZE : int = 16
-total_batches = total_slices//BATCH_SIZE
-IMG_SIZE : int = 512
+MODE = "CT"
+BATCH_SIZE = 16
+IMG_SIZE = 512
 DATASET_RATIOS = {
     "train" : 0.90,
     "test" : 0.05,
     "val" : 0.05
 }
 
-TRAIN_BATCHES = int(total_batches*DATASET_RATIOS["train"])
-TEST_BATCHES = int(total_batches*DATASET_RATIOS["test"])
-VAL_BATCHES = int(total_batches*DATASET_RATIOS["val"])
+def count_total_slices(sources, mode):
+    total_slices = 0
+    loop = tqdm(range(len(sources)))
+    for i in loop:
+        try:
+            nii_ = nib.load(
+                sources.iloc[i]["FinalPath"]
+            )
+            image = nii_.get_fdata()
+        except:
+            continue
+        if mode == "MRI":
+            if len(image.shape) > 3 and np.min(image) < 0:
+                continue
+        total_slices += min(image.shape)
+    return total_slices
 
-if mode == "CT":
-    LOWER_LIMIT = -1024
-    UPPER_LIMIT = 3071
-else:
-    LOWER_LIMIT = 0
-    UPPER_LIMIT = 2000
-
-def winsorize_and_rescale(image):
-    x = np.where(image < LOWER_LIMIT, LOWER_LIMIT, image)
-    x = np.where(x > UPPER_LIMIT, UPPER_LIMIT, x)
-    x = (x - LOWER_LIMIT) / (UPPER_LIMIT - LOWER_LIMIT)
+def winsorize_and_rescale(image, limits):
+    lower_limit, upper_limit = limits
+    x = np.where(image < lower_limit, lower_limit, image)
+    x = np.where(x > upper_limit, upper_limit, x)
+    x = (x - lower_limit) / (upper_limit - lower_limit)
     return x
 
 def resize(x):
@@ -91,19 +81,39 @@ def create_mmap(dataset, mode, shape):
     )
     return memmap_array
 
+sources = pd.read_csv(dataset_root, MODE)
+
+total_slices = count_total_slices(sources)
+print(f"Total slices: {total_slices}")
+
+
+total_batches = total_slices//BATCH_SIZE
+
+
+TRAIN_BATCHES = int(total_batches*DATASET_RATIOS["train"])
+TEST_BATCHES = int(total_batches*DATASET_RATIOS["test"])
+VAL_BATCHES = int(total_batches*DATASET_RATIOS["val"])
+
+if MODE == "CT":
+    LOWER_LIMIT = -1024
+    UPPER_LIMIT = 3071
+else:
+    LOWER_LIMIT = 0
+    UPPER_LIMIT = 2000
+
 train_mmap = create_mmap(
     "train",
-    mode,
+    MODE,
     (TRAIN_BATCHES, BATCH_SIZE, 1, IMG_SIZE, IMG_SIZE)
 )
 test_mmap = create_mmap(
     "test",
-    mode,
+    MODE,
     (TEST_BATCHES, BATCH_SIZE, 1, IMG_SIZE, IMG_SIZE)
 )
 val_mmap = create_mmap(
     "val",
-    mode,
+    MODE,
     (VAL_BATCHES, BATCH_SIZE, 1, IMG_SIZE, IMG_SIZE)
 )
 
@@ -124,7 +134,7 @@ for i in loop:
     try:
         nii_data = nii_.get_fdata().astype(np.float32)
     except:continue
-    if mode == "MRI" and np.min(nii_data) < 0:
+    if MODE == "MRI" and np.min(nii_data) < 0:
         continue
     if len(nii_data.shape) > 3:
         continue
