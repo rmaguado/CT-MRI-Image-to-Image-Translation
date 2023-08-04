88bc3522d0b2471e18656c0029bccaf4f86b6e9d
rmaguado <rmorenoaguado3643@gmail.com>
2023-08-04 11:27:36 +0200

    reorganize dataset, dataloader; size limits

---
+++ dataloaders.py
@@ -0,0 +1,125 @@
+from typing import Optional
+import numpy as np
+from glob import glob
+import os
+
+class Dataset:
+    def __init__(
+            self,
+            data_root_path: str,
+            dataset: str = "train",
+            mode: str = "CT",
+            enable_data_augmentation: bool = False
+        ):
+        dataset_path = os.path.join(data_root_path, dataset)
+        self.enable_data_augmentation = enable_data_augmentation
+
+        paths_to_database = glob(
+            os.path.join(dataset_path, mode, "*.npy")
+        )
+        if not paths_to_database:
+            raise ValueError("Data not found. Check source directory provided.")
+
+        data_dir = paths_to_database[0]
+        data_shape = tuple(
+            [
+                int(x) for x in os.path.basename(
+                    data_dir
+                ).replace(".npy","").split("_")
+            ]
+        )
+        self.batches = np.memmap(
+            data_dir,
+            dtype=np.float32,
+            mode='r',
+            shape=data_shape
+        )
+        self.counter = 0
+        self.total_batches = data_shape[0]
+
+    def data_augmentation(self, batch_data):
+        """Reflects and rotates the image at random.
+
+        Args:
+            batch_data (np.ndarray): Shape (B, C, H, W)
+        Returns:
+            (nd.array) : Transformed batch_data (B, C, H, W)
+        """
+        num_rotations = np.random.randint(4)
+        batch_data = np.rot90(batch_data, num_rotations, axes=(2, 3))
+
+        if np.random.random() < 0.5:
+            batch_data = np.flip(batch_data, axis=3)
+
+        return batch_data
+    def __getitem__(self, idx):
+        return self.batches[idx]
+    def __len__(self):
+        return self.total_batches
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.counter == self.total_batches:
+            self.counter = 0
+        batch = self.batches[self.counter]
+        self.counter += 1
+        if self.enable_data_augmentation:
+            return self.data_augmentation(batch)
+        return batch
+    
+class Dataloader:
+    """Pools together MRI and CT scans for masked modeling.
+    
+    Assumes that data_root_path contains a directory called dataset, which
+    contains two subdirectories, CT and MR, which contain .npy files. The names
+    of the .npy files should contain the shape of the data, e.g. 
+    100_1_256_256.npy. The number of batches in each dataset does not need to be
+    the same. 
+    
+    Args:
+        data_root_path (str): Path to the directory containing the dataset
+        dataset_name (str): Name of the dataset to load. e.g. "train" or "test"
+    """
+    def __init__(
+            self,
+            data_root_path: str,
+            dataset_name: str = "train",
+            size_limit: Optional[int] = None
+        ):
+        self.loaders = [
+            Dataset(data_root_path, dataset_name, "CT"),
+            Dataset(data_root_path, dataset_name, "MR")
+        ]
+        if size_limit is None:
+            self.loop_length = min(
+                [len(x) for x in self.loaders]
+            ) * 2
+        else:
+            self.loop_length = size_limit
+        self.mode = True
+        self.counter = 0
+        self.masking_ratio = 0.75
+    def __len__(self):
+        return self.loop_length
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.mode = not self.mode
+        if self.counter == self.loop_length:
+            self.counter = 0
+        next_item = next(self.loaders[self.mode])
+        mode = ["CT", "MR"][self.mode]
+        return next_item, mode
+
+if __name__ == "__main__":
+    from tqdm import tqdm
+    import time
+    t0 = time.time()
+    source = "/nfs/home/clruben/workspace/nst/data/preprocessed/"
+    testloader = Dataset(source, "train", "CT")
+    print(time.time()-t0)
+    t0 = time.time()
+    loop = tqdm(range(len(testloader)))
+    for batch in loop:
+        x = next(testloader)
+    print(time.time()-t0)
--- models/ViT_MAE/train.py
+++ models/ViT_MAE/train.py
@@ -1,51 +1,25 @@
 import json
 from torch.optim import AdamW
-from dataloader import Dataloader
 
+from dataloaders import Dataloader
 from models.ViT_MAE.vit_translation import ViT_Translation
 from trainer import Trainer
 
-rootpath = "/nfs/home/clruben/workspace/nst/data/"
+data_root_path = "/nfs/home/clruben/workspace/nst/data/"
 config_path = "/nfs/home/clruben/workspace/nst/models/ViT_MAE/config.json"
 
 with open(config_path) as file:
     config = json.load(file)
 lr = config["trainer"]["learning_rate"]
 
-class MAE_Dataloader:
-    def __init__(self, source, dataset):
-        """Pools together MRI and CT scans for masked modeling.
-        """
-        self.loaders = [
-            Dataloader(source, dataset, "CT"),
-            Dataloader(source, dataset, "MR")
-        ]
-        self.equal_num_examples = min(
-            [len(x) for x in self.loaders]
-        ) * 2
-        self.mode = True
-        self.counter = 0
-        self.masking_ratio = 0.75
-    def __len__(self):
-        return self.equal_num_examples
-    def __iter__(self):
-        return self
-    def __next__(self):
-        self.mode = not self.mode
-        if self.counter == self.equal_num_examples:
-            self.counter = 0
-        next_item = next(self.loaders[self.mode])
-        mode = ["CT", "MR"][self.mode]
-        return next_item, mode
-
-train_loader = MAE_Dataloader(
-    rootpath,
+train_loader = Dataloader(
+    data_root_path,
     'train'
 )
-
-test_loader = MAE_Dataloader(
-    rootpath,
-    'test'
+test_loader = Dataloader(
+    data_root_path,
+    'test',
+    size_limit=500
 )
 
 model = ViT_Translation(**config["model"])
