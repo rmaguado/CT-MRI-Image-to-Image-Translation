7fd7ad0248c93d2d55f4d5f7397545167d690e76
rmaguado <rmorenoaguado3643@gmail.com>
2023-08-10 12:06:33 +0200

    custom tensor logging

--- models/mae_gan/train.py
+++ models/mae_gan/train.py
@@ -2,11 +2,27 @@ import json
 from torch.optim import AdamW
 
 from dataloaders import Dataloader
-from models.vit_mae.vit_translation import VitTranslation
+from models.mae_gan.mae_gan import MaeGan
 from trainer import Trainer
 
+class MaeGanTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    def train_tensorboard(self, outputs):
+        loss = outputs.reconstruction_loss
+        self.writer.add_scalar(
+            "Loss/train",
+            loss.item(),
+            self.batch_counter
+        )
+        self.writer.add_scalar(
+            "Learning Rate",
+            self.optim.param_groups[0]['lr'],
+            self.batch_counter
+        )
+
 data_root_path = "/nfs/home/clruben/workspace/nst/data/"
-config_path = "/nfs/home/clruben/workspace/nst/models/vit_mae/config.json"
+config_path = "/nfs/home/clruben/workspace/nst/models/mae_gan/config.json"
 
 with open(config_path) as file:
     config = json.load(file)
@@ -16,22 +32,17 @@ train_loader = Dataloader(
     data_root_path,
     'train'
 )
-test_loader = Dataloader(
-    data_root_path,
-    'test',
-    size_limit=500
-)
 
-model = VitTranslation(**config["model"])
+model = MaeGan(**config["model"])
 optim = AdamW(model.parameters(), lr=lr)
 
-trainer = Trainer(
+trainer = MaeGanTrainer(
     model, 
     optim, 
     config, 
     **config["trainer"]
 )
 
-trainer.train(train_loader, test_loader)
+trainer.train(train_loader)
 #model.mode = "translation"
 #trainer.train(testloader, testloader)
